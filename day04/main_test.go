ab601089be1be232f1d7ac08535b7f2a9fdc9418
Austin Parker <austin@ap2.io>
2025-12-02 07:08:24 -0800

    add tests

---
+++ day04/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestPart1(t *testing.T) {
+	input := []string{
+		// TODO: add example input
+	}
+	expected := 0 // TODO: expected result
+	result := part1(input)
+	if result != expected {
+		t.Errorf("part1() = %d, want %d", result, expected)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	input := []string{
+		// TODO: add example input
+	}
+	expected := 0 // TODO: expected result
+	result := part2(input)
+	if result != expected {
+		t.Errorf("part2() = %d, want %d", result, expected)
+	}
+}
