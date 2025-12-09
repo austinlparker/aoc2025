9a175eb1dd477e337580ac38837717ef83a9e729
Austin Parker <austin@ap2.io>
2025-12-09 11:26:22 -0500

    day 9

--- day09/main.go
+++ day09/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -20,12 +21,113 @@ func readInput(filename string) []string {
 	return strings.Split(strings.TrimSuffix(string(data), "\n"), "\n")
 }
 
+type Point struct {
+	x, y int
+}
+
+func parsePoints(lines []string) []Point {
+	points := make([]Point, len(lines))
+	for i, line := range lines {
+		parts := strings.Split(line, ",")
+		points[i].x, _ = strconv.Atoi(parts[0])
+		points[i].y, _ = strconv.Atoi(parts[1])
+	}
+	return points
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func area(p1, p2 Point) int {
+	return (abs(p1.x-p2.x) + 1) * (abs(p1.y-p2.y) + 1)
+}
+
 func part1(lines []string) int {
-	// TODO: implement
-	return 0
+	points := parsePoints(lines)
+
+	maxArea := 0
+	for i := range len(points) {
+		for j := i + 1; j < len(points); j++ {
+			maxArea = max(maxArea, area(points[i], points[j]))
+		}
+	}
+	return maxArea
+}
+
+type Outline map[Point]bool
+
+func (o Outline) paint(from, to Point) {
+	if from.x == to.x {
+		for y := min(from.y, to.y); y <= max(from.y, to.y); y++ {
+			o[Point{from.x, y}] = true
+		}
+	} else if from.y == to.y {
+		for x := min(from.x, to.x); x <= max(from.x, to.x); x++ {
+			o[Point{x, from.y}] = true
+		}
+	}
+}
+
+func buildOutline(points []Point) Outline {
+	outline := make(Outline)
+	n := len(points)
+	for i := range n {
+		outline.paint(points[i], points[(i+1)%n])
+	}
+	return outline
 }
 
 func part2(lines []string) int {
-	// TODO: implement
-	return 0
+	points := parsePoints(lines)
+	outline := buildOutline(points)
+
+	maxArea := 0
+	for i, a := range points {
+	outer:
+		for j, b := range points {
+			if i >= j {
+				continue
+			}
+
+			candidateArea := area(a, b)
+			if candidateArea <= maxArea {
+				continue
+			}
+
+			xLo, xHi := min(a.x, b.x), max(a.x, b.x)
+			yLo, yHi := min(a.y, b.y), max(a.y, b.y)
+
+			for k, c := range points {
+				if k == i || k == j {
+					continue
+				}
+				if xLo < c.x && c.x < xHi && yLo < c.y && c.y < yHi {
+					continue outer
+				}
+			}
+
+			for r, x := range [2]int{xLo, xHi} {
+				for y := yLo + 1; y <= yHi-1; y++ {
+					if outline[Point{x + 1 - 2*r, y}] {
+						continue outer
+					}
+				}
+			}
+
+			for s, y := range [2]int{yLo, yHi} {
+				for x := xLo + 1; x <= xHi-1; x++ {
+					if outline[Point{x, y + 1 - 2*s}] {
+						continue outer
+					}
+				}
+			}
+
+			maxArea = candidateArea
+		}
+	}
+	return maxArea
 }
--- day09/main_test.go
+++ day09/main_test.go
@@ -4,9 +4,16 @@ import "testing"
 
 func TestPart1(t *testing.T) {
 	input := []string{
-		// TODO: add example input
+		"7,1",
+		"11,1",
+		"11,7",
+		"9,7",
+		"9,5",
+		"2,5",
+		"2,3",
+		"7,3",
 	}
-	expected := 0 // TODO: expected result
+	expected := 50
 	result := part1(input)
 	if result != expected {
 		t.Errorf("part1() = %d, want %d", result, expected)
@@ -15,11 +22,34 @@ func TestPart1(t *testing.T) {
 
 func TestPart2(t *testing.T) {
 	input := []string{
-		// TODO: add example input
+		"7,1",
+		"11,1",
+		"11,7",
+		"9,7",
+		"9,5",
+		"2,5",
+		"2,3",
+		"7,3",
 	}
-	expected := 0 // TODO: expected result
+	expected := 24
 	result := part2(input)
 	if result != expected {
 		t.Errorf("part2() = %d, want %d", result, expected)
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	lines := readInput("input.txt")
+	b.ResetTimer()
+	for range b.N {
+		part1(lines)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	lines := readInput("input.txt")
+	b.ResetTimer()
+	for range b.N {
+		part2(lines)
 	}
 }
